15299c3f86b05b9e7eb6016f47d16499cc434aa1
Farhan Mac <farhan.fadila1717@gmail.com>
2022-04-12 13:11:52 +0700

    refactor template

--- mason/bricks/page_template_ns/__brick__/{{name}}_page/{{name}}_page_action_mapper.dart
+++
@@ -1,6 +0,0 @@
-import 'package:client_app/core/redux/action_mapper.dart';
-import 'package:redux/redux.dart';
-
-class {{#pascalCase}}{{name}}{{/pascalCase}}PageActionMapper extends ActionMapper {
-  {{#pascalCase}}{{name}}{{/pascalCase}}PageActionMapper(Store store) : super(store);
-}
--- mason/page_template/__brick__/{{name}}_page/{{name}}_page.dart
+++ mason/page_template/__brick__/{{name}}_page/{{name}}_page.dart
@@ -49,6 +49,10 @@ class _{{#pascalCase}}{{name}}{{/pascalCase}}PageState extends State<{{#pascalCa
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text({{name}}),
+      ),
+    );
   }
 }
