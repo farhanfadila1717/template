237998a0ca5c49a73400d73141a92c91696000e8
Farhan Mac <farhan.fadila1717@gmail.com>
2022-07-08 15:56:39 +0700

    Fixed optional paramater

--- mason/page_template/CHANGELOG.md
+++ mason/page_template/CHANGELOG.md
@@ -1,3 +1,2 @@
-# 0.1.0+1
-
-- TODO: Describe initial release.
+# 0.1.0+3
+* Fixed optional parameter
--- mason/page_template/__brick__/{{name}}_page/{{name}}_page.dart
+++ mason/page_template/__brick__/{{name}}_page/{{name}}_page.dart
@@ -3,7 +3,6 @@ import 'package:client_app/core/di/injectable_widget.dart';
 import 'package:client_app/ui/utils/color_palette.dart';
 import 'package:flutter/material.dart';
 import 'package:redux/redux.dart';
-import 'package:client_app/core/utils/utils.dart';
 
 import 'di/{{name}}_page_graph.dart';
 import '{{name}}_page_action_mapper.dart';
@@ -26,13 +25,16 @@ class {{#pascalCase}}{{name}}{{/pascalCase}}Page extends StatefulWidget with Inj
   @override
   {{#pascalCase}}{{name}}{{/pascalCase}}PageGraph graph() => _graph;
 
-  {{#pascalCase}}{{name}}{{/pascalCase}}Page([graph])
+  {{#pascalCase}}{{name}}{{/pascalCase}}Page({
+    Key key,
+    {{#pascalCase}}{{name}}{{/pascalCase}}PageGraph graph,
+  })
       : this._graph = graph ?? {{#pascalCase}}{{name}}{{/pascalCase}}PageGraph() {
     setup();
   }
 
   @override
-  _{{#pascalCase}}{{name}}{{/pascalCase}}PageState createState() => _{{#pascalCase}}{{name}}{{/pascalCase}}PageState();
+  createState() => _{{#pascalCase}}{{name}}{{/pascalCase}}PageState();
 }
 
 class _{{#pascalCase}}{{name}}{{/pascalCase}}PageState extends State<{{#pascalCase}}{{name}}{{/pascalCase}}Page> {
@@ -51,7 +53,7 @@ class _{{#pascalCase}}{{name}}{{/pascalCase}}PageState extends State<{{#pascalCa
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text({{name}}),
+        child: Text("{{#pascalCase}}{{name}}{{/pascalCase}}Page"),
       ),
     );
   }
